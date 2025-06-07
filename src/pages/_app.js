c716e51af5a1981f84af34a8e57cc91599b3db0d
shreyansh <shreyansh@codalent.com>
2025-06-07 11:09:40 +0530

    feat: added color palette

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-Plus_Jakarta_Sans",
+  weight: ["400", "500", "600"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${plusJakartaSans.variable} font-body`}>
+      <style jsx global>{`
+        html {
+          font-family: ${plusJakartaSans.style.fontFamily}, ui-sans-serif,
+            system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            "Helvetica Neue", Arial, "Noto Sans", sans-serif,
+            "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+            "Noto Color Emoji";
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,89 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: { xs: "475px", xxs: "200px", ...defaultTheme.screens },
+    fontFamily: {
+      sans: ["--font-Plus_Jakarta_Sans", ...fontFamily.sans],
+      headline: ["--font-Plus_Jakarta_Sans", ...fontFamily.sans],
+      body: ["--font-Plus_Jakarta_Sans", ...fontFamily.sans],
+    },
+
+    fontSize: {
+      xxs: ["0.625rem", { lineHeight: "0.75rem" }], // 10px
+      xs: ["0.75rem", { lineHeight: "1rem" }], // 12px
+      sm: ["0.875rem", { lineHeight: "1.25rem" }], // 14px
+      base: ["1rem", { lineHeight: "1.5rem" }], // 16px
+      lg: ["1.125rem", { lineHeight: "1.75rem" }], // 18px
+      xl: ["1.25rem", { lineHeight: "1.75rem" }], // 20px
+      "1.5xl": ["1.375rem", { lineHeight: "1.875rem" }], // 22px
+      "2xl": ["1.5rem", { lineHeight: "2rem" }], // 24px
+      "2.5xl": ["1.75rem", { lineHeight: "2.25rem" }], // 28px
+      "3xl": ["1.875rem", { lineHeight: "2.25rem" }], // 30px
+      "3.5xl": ["2rem", { lineHeight: "2.5rem" }], // 32px
+      "4xl": ["2.25rem", { lineHeight: "2.5rem" }], // 36px
+      "4.5xl": ["2.5rem", { lineHeight: "2.75rem" }], // 40px
+      "5xl": ["3rem", { lineHeight: "3.625rem" }], // 48px
+      "6xl": ["3.875rem", { lineHeight: "1" }], // 62px
+      "7xl": ["4.5rem", { lineHeight: "1" }], // 72px
+      "8xl": ["6rem", { lineHeight: "1" }], // 96px
+      "9xl": ["8rem", { lineHeight: "1" }], // 128px
+    },
+    extend: {
+      colors: {
+        brand: {
+          blue: {
+            DEFAULT: "#0d6efd",
+            100: "#0dcaf0",
+            500: "#0d6efd",
+          },
+          indigo: {
+            DEFAULT: "#6610f2",
+            400: "#6f42c1",
+            500: "#6610f2",
+          },
+          pink: {
+            DEFAULT: "#d63384",
+            500: "#d63384",
+          },
+          red: {
+            DEFAULT: "#dc3545",
+            500: "#dc3545",
+          },
+          orange: {
+            DEFAULT: "#fd7e14",
+            500: "#fd7e14",
+          },
+          yellow: {
+            DEFAULT: "#ffc107",
+            500: "#ffc107",
+          },
+          green: {
+            DEFAULT: "#198754",
+            300: "#20c997",
+            500: "#198754",
+          },
+          gray: {
+            DEFAULT: "#343a40",
+            300: "#6c757d",
+            500: "#343a40",
+            700: "#212529",
+            800: "#1a1a1a",
+          },
+          neutral: {
+            DEFAULT: "#1a1a1a",
+            100: "#f8f9fa",
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
